3c0df6fb8fb3b33bd5e99376c78340edc21a8640
ie6smash@gmail.com <ie6smash@gmail.com@e2767d01-9978-bd9b-7b41-756c6960b13b>
2013-04-22 05:49:35 +0000

    Fixed a small bug regarding CMPR texture size. Renamed FileHander to IsoFile. Began working on the DatNode class which will replace node structs.

--- trunk/MeleeToolkit/IsoFile.cs
+++ trunk/MeleeToolkit/IsoFile.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace MeleeToolkit
 {
-    static class FileHandler
+    static class IsoFile
     {
         private static FileStream _streamFile;
         private const int OffsetID = 0x0;
